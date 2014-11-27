782b08d87d22e2060b53bbff46b73d27fdf9a1c9
Kit Barnes <k.barnes@mhnltd.co.uk>
2014-11-27 19:49:20 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from setuptools import setup
+
+setup(name='dockerfeed',
+      version='0.1',
+      packages=['dockerfeed'],
+      zip_safe=True,
+      scripts=['bin/dockerfeed'],
+      )
